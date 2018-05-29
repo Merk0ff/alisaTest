c7a233788d71a29f91e6287f8caf5a8f54eae834
Philip Dukshtau [PD6] <f.dukshtau@gmail.com>
2018-05-29 13:03:07 +0300

    Heroku

--- python/buy-elephant/azure/main.py
+++ python/buy-elephant/azure/main.py
@@ -1,12 +1,9 @@
 # coding: utf-8
-# Импортирует поддержку UTF-8.
 from __future__ import unicode_literals
 
-# Импортируем модули для работы с JSON и логами.
 import json
 import logging
 
-# Импортируем подмодули Flask для запуска веб-сервиса.
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -15,31 +12,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Хранилище данных о сессиях.
 sessionStorage = {}
-
-# Задаем параметры приложения Flask.
-@app.route("/", methods=['POST'])
-
-def main():
-# Функция получает тело запроса и возвращает ответ.
-    logging.info('Request: %r', request.json)
-
-    response = {
-        "version": request.json['version'],
-        "session": request.json['session'],
-        "response": {
-            "end_session": False
-        }
-    }
-
-    handle_dialog(request.json, response)
-
-    logging.info('Response: %r', response)
-
-    return json.dumps(
-        response,
-        ensure_ascii=False,
-        indent=2
-    )
 
 # Функция для непосредственной обработки диалога.
 def handle_dialog(req, res):
@@ -102,3 +74,28 @@ def getSuggests(user_id):
         })
 
     return suggests
+
+# Задаем параметры приложения Flask.
+@app.route("/", methods=['POST'])
+
+def main():
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request: %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
